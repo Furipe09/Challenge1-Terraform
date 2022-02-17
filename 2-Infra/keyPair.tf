48ebed217c8e58470012f5a366c2d67e17fabed0
Felipe Vieira <felipe09@gmail.com>
2022-02-16 22:22:00 -0300

    inclusao de valores default no variables.tf

--- 2-Infra/keyPair.tf
+++ 2-Infra/keyPair.tf
@@ -6,6 +6,6 @@ module "key_pair" {
   source = "terraform-aws-modules/key-pair/aws"
 
   key_name   = "deploy-terraform"
-  public_key = file("/home/furion/.ssh/id_rsa.pub") 
+  public_key = file("/home/furion/.ssh/id_rsa.pub")
   #tls_private_key.this.public_key_openssh
 }
--- 2-Infra/variables.tf
+++ 2-Infra/variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = ""
   type        = string
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
@@ -12,30 +13,39 @@ variable "aws_profile" {
 variable "aws_amis_ec2" {
   description = ""
   type        = string
+  default     = "ami-04505e74c0741db8d"
 }
 
 variable "aws_instance_type_ec2" {
   description = ""
   type        = string
+  default     = "t2.micro"
 }
 
 variable "aws_key_name_ec2" {
   description = ""
   type        = string
+  default     = "deploy-terraform"
 }
 
 variable "aws_associate_public_ip_address_ec2" {
   description = ""
   type        = bool
+  default     = true
 }
 
 variable "aws_instance_tags" {
   description = ""
   type        = map(string)
+  default = {
+    Provider = "terraform"
+    Owner    = "Felipe Vieira"
+    Version  = "14-02/2022"
+  }
 }
 
 variable "aws_nginx_ec2" {
   description = ""
   type        = string
-  default     = ""
+  default     = "./nginx.sh"
 }
