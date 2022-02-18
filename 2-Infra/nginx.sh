d074df6aba336a94dd5440c25dea33e0085b2c8e
Furion <31106638+Furipe09@users.noreply.github.com>
2022-02-18 11:48:29 -0300

    Update nginx.sh

--- 2-Infra/nginx.sh
+++ 2-Infra/nginx.sh
@@ -3,8 +3,3 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-#sudo ufw allow 'Nginx HTTP'
-#sudo ufw allow 22
-#sudo systemctl start ufw
-#sudo ufw enable
-#echo "AWS Instance created by Terraform"
